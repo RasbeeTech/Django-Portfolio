160fba3a1f5df353ac263ac7efd4f50a98dfb9b3
MykalB081 <mykalb081@gmail.com>
2021-04-06 13:22:32 -0400

    Update Portolio

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MykalBailey',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
